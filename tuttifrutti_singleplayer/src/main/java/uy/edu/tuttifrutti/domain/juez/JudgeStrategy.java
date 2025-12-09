0fc1e65f02e7d62fc29663603f702ff749672255
AgustinEtchepare <dagustinechpare@gmail.com>
2025-12-08 22:20:40 -0300

    ia arreglada

--- tuttifrutti_singleplayer/src/main/java/uy/edu/tuttifrutti/domain/juez/JudgeStrategy.java
+++
@@ -1,9 +0,0 @@
-package uy.edu.tuttifrutti.domain.juez;
-
-import uy.edu.tuttifrutti.domain.config.GameConfig;
-import uy.edu.tuttifrutti.domain.juego.RoundSubmission;
-
-public interface JudgeStrategy {
-
-    JudgeResult juzgar(RoundSubmission submission, GameConfig config);
-}
